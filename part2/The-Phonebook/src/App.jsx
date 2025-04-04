855b8d0ff30ef852ba3cbf8bb561128959ff8e03
guy-car <guil.crn@gmail.com>
2025-04-04 19:09:38 -0400

    Phonebook with JSON Server

--- part2/The-Phonebook/src/App.jsx
+++ part2/The-Phonebook/src/App.jsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Search from "./components/Search"
 import Form from "./components/Form"
 import Content from "./components/Content"
+import axios from 'axios'
 
 const App = () => {
 
   // state values
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState("")
   const [newFilter, setNewFilter] = useState("")
+
+  // Server data
+  useEffect( () => {
+    console.log("effect ran");
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
+  console.log("content of persons state: ", persons)
+
 
   // When user clicks "add" run the below
   const addPerson = (event) => {
